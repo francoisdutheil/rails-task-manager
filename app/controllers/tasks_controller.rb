97df23a688eea8dc094b2216b873d261b734e224
francois dutheil <frdutheil@yahoo.fr>
2019-02-12 18:30:07 +0100

    task manager evening next to do delete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,30 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id',  to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit' , to: 'tasks#edit', as: :edit
 end
+
+
+#   restaurants GET    /restaurants(.:format)          restaurants#index
+#                POST   /restaurants(.:format)          restaurants#create
+# new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+#edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#     restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                PATCH  /restaurants/:id(.:format)      restaurants#update
+#                DELETE /restaurants/:id(.:format)      restaurants#destroy
